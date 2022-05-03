621ccf9d069e1935c8c8baf407de6ada8886f4c6
nehabht <ne.bhatti2@icloud.com>
2022-05-03 15:45:44 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="container80 navbar">
+      <img class="logo_img" src="./assets/img/dc-logo.png" alt="">
+      <nav>
+        <ul>
+          <li>Characters</li>
+          <li class="active">Comics</li>
+          <li>Movies</li>
+          <li>Tv</li>
+          <li>Games</li>
+          <li>Collectibles</li>
+          <li>Videos</li>
+          <li>Fans</li>
+          <li>News</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      content here
+    </main>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
@@ -23,6 +43,59 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+
+
+.container80{
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 1rem;
+
+}
+
+.navbar{
+  height: 100px;
+  align-items: center;
+}
+
+.logo_img{
+  height: 60px;
+  width: 60px;
+}
+
+ul{
+  list-style: none;
+    align-content: center;
+}
+
+li{
+  display: inline;
+  text-transform: uppercase;
+  padding: 1rem;
+  font-weight: 700;
+  font-size: 11px;
+}
+
+.active{
+  color: blue;
+
+}
+
+//main
+
+main{
+  width: 100%;
+  background-color: black;
+  height: 100px;
+  color: white;
+}
+
 </style>
