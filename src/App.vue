6231a94e0f9296beb4f12e9870cf724b50b627bc
nehabht <ne.bhatti2@icloud.com>
2022-05-03 15:54:28 +0200

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
   <div id="app">
-    <header class="container80 navbar">
-      <img class="logo_img" src="./assets/img/dc-logo.png" alt="">
-      <nav>
-        <ul>
-          <li>Characters</li>
-          <li class="active">Comics</li>
-          <li>Movies</li>
-          <li>Tv</li>
-          <li>Games</li>
-          <li>Collectibles</li>
-          <li>Videos</li>
-          <li>Fans</li>
-          <li>News</li>
-          <li>Shop</li>
-        </ul>
-      </nav>
-    </header>
+    <headerComponent />
+    
 
     <main>
       content here
@@ -27,10 +12,12 @@
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-
+import headerComponent from './components/Header.vue'
 export default {
   name: 'App',
   components: {
+    headerComponent,
+
 
   }
 }
@@ -48,45 +35,6 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-
-}
-
-
-.container80{
-  width: 80%;
-  display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  padding: 1rem;
-
-}
-
-.navbar{
-  height: 100px;
-  align-items: center;
-}
-
-.logo_img{
-  height: 60px;
-  width: 60px;
-}
-
-ul{
-  list-style: none;
-    align-content: center;
-}
-
-li{
-  display: inline;
-  text-transform: uppercase;
-  padding: 1rem;
-  font-weight: 700;
-  font-size: 11px;
-}
-
-.active{
-  color: blue;
-
 }
 
 //main
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <header class="container80 navbar">
+      <img class="logo_img" src="../assets/img/dc-logo.png" alt="">
+      <nav>
+        <ul>
+          <li>Characters</li>
+          <li class="active">Comics</li>
+          <li>Movies</li>
+          <li>Tv</li>
+          <li>Games</li>
+          <li>Collectibles</li>
+          <li>Videos</li>
+          <li>Fans</li>
+          <li>News</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.container80{
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 1rem;
+
+}
+
+.navbar{
+  height: 100px;
+  align-items: center;
+}
+
+.logo_img{
+  height: 60px;
+  width: 60px;
+}
+
+ul{
+  list-style: none;
+    align-content: center;
+}
+
+li{
+  display: inline;
+  text-transform: uppercase;
+  padding: 1rem;
+  font-weight: 700;
+  font-size: 11px;
+}
+
+.active{
+  color: blue;
+
+}
+
+</style>
