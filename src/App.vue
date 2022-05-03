5851fba4653af9c62e8e6be0a9059278aaa76355
nehabht <ne.bhatti2@icloud.com>
2022-05-03 16:58:34 +0200

    add buy section component

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,7 @@
       content here
     </main>
 
-    <div class="buy_section">
-      <div class="container70">
-        <div class="cards_buy">
-          <div class="card_buy">
-            <img src="./assets/img/buy-comics-digital-comics.png" alt="">
-            <p class="text_buy">digital comics</p>
-          </div>
-          <div class="card_buy">
-            <img src="./assets/img/buy-comics-merchandise.png" alt="">
-            <p class="text_buy">dc merchandise</p>
-          </div>
-          <div class="card_buy">
-            <img src="./assets/img/buy-comics-subscriptions.png" alt="">
-            <p class="text_buy">subscriptions</p>
-          </div>
-          <div class="card_buy">
-            <img src="./assets/img/buy-comics-shop-locator.png" alt="">
-            <p class="text_buy">comic shop locator</p>
-          </div>
-          <div class="card_buy">
-            <img src="./assets/img/buy-dc-power-visa.svg" alt="">
-            <p class="text_buy">dc power visa</p>
-          </div>
-        </div>
-
-
-      </div>
-    </div>
+    <buySectionComponent />
 
   </div>
 
@@ -41,10 +14,12 @@
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import headerComponent from './components/Header.vue'
+import buySectionComponent from './components/BuySection.vue'
 export default {
   name: 'App',
   components: {
     headerComponent,
+    buySectionComponent,
 
 
   }
@@ -88,37 +63,6 @@ main{
   color: white;
 }
 
-//buy section
 
-.img{
-  width: 100%;
-}
-
-.cards_buy{
-  display: flex;
-  height: 100px;
-
-
-}
-
-.card_buy{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-    flex-grow: 1;
-
-  
-    img{
-      max-height: 35px;
-    }
-
-    p{
-      color:white;
-      padding-left: 1rem;
-      text-transform: uppercase;
-      font-size: 12px;
-    }
-  
-}
 
 </style>
---
+++ src/components/BuySection.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="buy_section">
+      <div class="container70">
+        <div class="cards_buy">
+          <div class="card_buy">
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <p class="text_buy">digital comics</p>
+          </div>
+          <div class="card_buy">
+            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+            <p class="text_buy">dc merchandise</p>
+          </div>
+          <div class="card_buy">
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <p class="text_buy">subscriptions</p>
+          </div>
+          <div class="card_buy">
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <p class="text_buy">comic shop locator</p>
+          </div>
+          <div class="card_buy">
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <p class="text_buy">dc power visa</p>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.img{
+  width: 100%;
+}
+
+.cards_buy{
+  display: flex;
+  height: 100px;
+
+
+}
+
+.card_buy{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+    flex-grow: 1;
+
+  
+    img{
+      max-height: 35px;
+    }
+
+    p{
+      color:white;
+      padding-left: 1rem;
+      text-transform: uppercase;
+      font-size: 12px;
+    }
+  
+}
+
+
+</style>
