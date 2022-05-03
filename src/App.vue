1c257f077f3a95b4ebee0e8d778e1f38dbe8d334
nehabht <ne.bhatti2@icloud.com>
2022-05-03 18:03:37 +0200

    add bottom top

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,72 @@
 
     <buySectionComponent />
 
+    <footer>
+      <div class="footer_top">
+        <div class="container80">
+          <div class="row">
+             <div class="col">
+               <div class="col-4">
+                 <h4>DC COMICS</h4>
+                 <section>
+                   <ul>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                   </ul>
+                 </section>
+                 <h4>SHOP</h4>
+                 <section>
+                   <ul>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                   </ul>
+
+                 </section>
+               </div>
+               <div class="col-4">
+                 <h4>DC</h4>
+                 <section>
+                   <ul>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                   </ul>
+                 </section>
+               </div>
+               <div class="col-4">
+                 <h4>SITES</h4>
+                 <section>
+                   <ul>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                      <li>lorem</li>
+                   </ul>
+                 </section>
+               </div>
+
+             </div>
+            <div class="col">
+              <img src="./assets/img/dc-logo-bg.png" alt="">
+            </div>
+          </div>
+        </div>
+       
+
+      </div>
+
+      <div class="footer_bottom"></div>
+    </footer>
+
   </div>
 
 </template>
@@ -31,7 +97,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
 }
 *{
@@ -40,8 +106,10 @@ export default {
   box-sizing: border-box;
 }
 
+
 .container80{
   width: 80%;
+  margin: 0 auto;
 }
 
 .container70{
@@ -49,6 +117,10 @@ export default {
   background-color: transparent;
   height: 100px;
   margin: 0 auto;
+}
+
+.row{
+  display: flex;
 }
 
 .buy_section{
@@ -61,6 +133,46 @@ main{
   background-color: black;
   height: 100px;
   color: white;
+}
+
+//footer top
+
+
+
+.footer_top{
+  width: inherit;
+  height: 400px;
+  background-image: url(./assets/img/footer-bg.jpg);
+
+  color: white;
+  display: flex;
+
+    .col{
+      display: flex;
+      width: 50%;
+      flex-grow: 1;
+
+        img{
+          height: 400px;
+
+        }
+         .col-4{
+          padding-top: 1rem ;
+          width: 25%;
+        }
+    }
+}
+
+h4{
+  margin: 1rem 0;
+}
+
+ul{
+  list-style: none;
+}
+li{
+  font-size: 10px;
+  color: lightgray
 }
 
 
